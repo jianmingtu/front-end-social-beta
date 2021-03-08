189209d58ae07ff56185d10d02603f37d290650a
Wayne Lee <waynel1996weiyang@gmail.com>
2021-03-08 13:31:56 -0800

    Add profile card (which is not a card) in UserProfile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from 'react'
 
+import styles from './UserProfile.module.css'
+
 export default function UserProfile() {
   return (
-    "Profile"
+    <div className={styles.container}>
+      <div className={styles.profileContainer}>
+        <span className={styles.profileUser}>
+          <span className={styles.user}>
+            <img className={styles.avatar} src="https://cdn.discordapp.com/attachments/738356484462608424/816066240917405716/unknown.png" />
+            <p>Seal</p>
+          </span>
+          <button>Edit</button>
+        </span>
+        <span className={styles.profileContent}>
+          <p>Post Content here</p>
+        </span>
+      </div>
+    </div>
+    // Filter Post here if you want
   )
 }
