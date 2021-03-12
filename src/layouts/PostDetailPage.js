23b2221268ecda29fce25c263fcf52fac3c99fb0
Wayne Lee <waynel1996weiyang@gmail.com>
2021-03-12 14:11:46 -0800

    Hook up the network function to appropriate layouts

--- src/layouts/PostDetailPage.js
+++ src/layouts/PostDetailPage.js
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import PostDetail from '../components/PostDetailPage/PostDetail'
+import { getPost, getComment, createComment } from '../network/network'
 
 export default function PostDetailPage() {
+  const [post, setPost] = useState()
+  const [comments, setComment] = useState()
+
+  let { postId } = useParams()
+
+  const submitComment = async data => {
+    await createComment({data, postId: postId})
+  }
+
+  useEffect(() => {
+    (async () => {
+        const resultPost = await getPost({postId: postId})
+        const resultComments = await getComment({postId: postId})
+        // setPost(resultPost.post)
+        // setComment(resultComments.comments)
+    })()
+  }, [])
+
   return (
     //pass in comment list here for it to be rendered
-    <PostDetail />
+    <PostDetail 
+      comments={comments}
+      submitComment={submitComment}
+    />
   )
 }
--- src/layouts/PostPage.js
+++ src/layouts/PostPage.js
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from 'react'
 
 import Post from '../components/PostPage/Post'
 import NewPostForm from '../components/PostPage/NewPostForm'
-import { createPost } from '../network/network'
+import { getPosts, createPost } from '../network/network'
 import styles from './Layout.module.css'
 
-const submitPost = async (data) => {
-  await createPost({data})
-  console.log(data)
-}
-
 export default function PostPage() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const result = await getPosts()
+      // setPosts(result.posts)
+    })()
+  }, [])
+
+  const submitPost = async (data) => {
+    await createPost({data})
+    console.log(data)
+  }
+
   return (
     <div className={styles.container}>
       <NewPostForm submitPost={submitPost} />
