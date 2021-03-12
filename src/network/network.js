4955d8391806e88b8722f4c63733c8e501787e54
jianmingtu <tu_jm@yahoo.com>
2021-03-12 11:35:04 -0800

    #26 add authHeader

--- src/network/network.js
+++ src/network/network.js
@@ -1,21 +1,35 @@
 import axios from 'axios'
-
 import { upload } from '../s3'
+import {userToken} from './userAuth'
 
-// const multer = require('multer')
-// const upload = multer({ dest: 'uploads/' })
+const BASE_API = "https://lpmp2m4ovd.execute-api.us-east-2.amazonaws.com/prod"
+
+// get user JWT token from the user Cognito's local storage and create an Authorization header.
+async function authHeader() {
+  const token = await userToken()
+  if (!token) {
+    return {}
+  }
+  return { Authorization: `Bearer ${token}` }
+}
 
 export async function createPost({data}) {
   try {
-    if(data.imageFile) {
-      upload(data.imageFile.files[0])
-    }
-    
+
+    console.log(data)
+
+    // if(data.imageFile) {
+    //   upload(data.imageFile.files[0])
+    // }
+
     // upload.single('image')
-    // const result = await axios.post(`/api/posts`, {
-    //   ...data
-    // }, { headers: authHeader })
+    
+    console.log(...data)
+    //Send the JWT in the header of the axios requests from the client
+    await axios.post(`/api/posts`, ...data
+    , { headers: authHeader() })
+
   } catch (error) {
-    console.log(error)
+    throw (error.response.data.error ? Error(error.response.data.error) : error)
   }
-}
+}  
