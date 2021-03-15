e30c90b817a34fdc3f02f81d5f6d333af6019cb0
jianmingtu <tu_jm@yahoo.com>
2021-03-15 07:20:39 -0700

    #26 - bug fix (  ...data threw exception so replace it with  content: data.content)

--- src/network/network.js
+++ src/network/network.js
@@ -3,6 +3,7 @@ import { upload } from '../s3'
 import {userToken} from './userAuth'
 
 const BASE_API = "https://lpmp2m4ovd.execute-api.us-east-2.amazonaws.com/prod"
+// const BASE_API = "http://localhost:5000/"
 
 // get user JWT token from the user Cognito's local storage and create an Authorization header.
 async function authHeader() {
@@ -28,7 +29,7 @@ export async function createPost({data}) {
     
     //Send the JWT in the header of the axios requests from the client
     const headers = await authHeader()
-    await axios.post(`${BASE_API}/posts`, { ...data, imageUrl: ret?.location} 
+    await axios.post(`${BASE_API}/posts`, { content: data.content, imageUrl: ret?.location} 
     , { headers})
 
   } catch (err) {
